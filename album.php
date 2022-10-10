972b1ff74efe973b98fc3866bdde89222ab13074
Dillon Rosenkranz <dillonnollid@gmail.com>
2022-10-10 21:27:14 +0100

    Lots of progress made on the album view page

--- album.php
+++ album.php
@@ -17,13 +17,12 @@ $artist = $album->getArtist();
 $artistId = $artist->getId();
 ?>
 
-<div class="entityInfo">
-	<!--Left sec for the image, right section to display title, link to Artist, number of songs in album -->
-	<div class="leftSection">
-		<img src="<?php echo $album->getArtworkPath(); ?>">
+<div class="entityInfo flex flex-col md:flex-row bg-green-100 justify-center items-center overflow-hidden m-8">
+	<div class="leftSection bg-blue-100 p-8 justify-center items-center mx-auto ">
+		<img src="<?php echo $album->getArtworkPath(); ?>" class="shadow-xl hover:shadow-2xl">
 	</div>
 
-	<div class="rightSection">
+	<div class="rightSection bg-blue-400 w-3/4 m-8 justify-center items-center text-center">
 		<h2><?php echo $album->getTitle(); ?></h2>
 		<p role="link" tabindex="0" onclick="openPage('artist.php?id=$artistId')">By <?php echo $artist->getName(); ?></p>
 		<p><?php echo $album->getNumberOfSongs(); ?> songs</p>
@@ -32,8 +31,8 @@ $artistId = $artist->getId();
 </div>
 
 <!-- List of all tracks in the album, get all song IDs from album, iterate through and output HTML -->
-<div class="tracklistContainer">
-	<ul class="tracklist">
+<div class="tracklistContainer flex-col bg-red-100 justify-center items-center w-auto overflow-hidden m-6 rounded-xl shadow-xs">
+	<ul class="tracklist list-none md:list-disc bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
 		
 		<?php
 		$songIdArray = $album->getSongIds();
@@ -44,25 +43,24 @@ $artistId = $artist->getId();
 			$albumSong = new Song($con, $songId);
 			$albumArtist = $albumSong->getArtist();
 
-			echo "<li class='tracklistRow'>
-					<div class='trackCount'>
-						<img class='play' src='assets/images/icons/play-white.png' onclick='setTrack(\"" . $albumSong->getId() . "\", tempPlaylist, true)'>
-						<span class='trackNumber'>$i</span>
+			echo "<li class='flex justify-between w-auto h-16 p-2'>
+					<div class='trackCount flex flex-row flex-auto basis-1/4'>
+					    <span class='trackNumber my-auto p-2 text-gray-700 dark:text-gray-400'>$i</span>
+						<img class='play my-auto w-10 h-10 p-2' src='assets/images/icons/play-white.png' onclick='setTrack(\"" . $albumSong->getId() . "\", tempPlaylist, true)'>
 					</div>
 
-
-					<div class='trackInfo'>
-						<span class='trackName'>" . $albumSong->getTitle() . "</span>
-						<span class='artistName'>" . $albumArtist->getName() . "</span>
+					<div class='trackInfo flex flex-col flex-4 basis-3/4 items-left'>
+						<span class='trackName font-medium h-1/2 text-gray-700 dark:text-gray-400'>" . $albumSong->getTitle() . "</span>
+						<span class='artistName font-light h-1/2 text-gray-700 dark:text-gray-400'>" . $albumArtist->getName() . "</span>
 					</div>
 
-					<div class='trackOptions'>
+					<div class='trackOptions flex flex-col flex-auto basis-1/4 items-center'>
 						<input type='hidden' class='songId' value='" . $albumSong->getId() . "'>
-						<img class='optionsButton' src='assets/images/icons/more.png' onclick='showOptionsMenu(this)'>
+						<img class='optionsButton w-8 h-8 my-auto' src='assets/images/icons/more.png' onclick='showOptionsMenu(this)'>
 					</div>
 
-					<div class='trackDuration'>
-						<span class='duration'>" . $albumSong->getDuration() . "</span>
+					<div class='trackDuration flex flex-col flex-2 basis-1/4 items-center'>
+						<span class='duration text-gray-700 dark:text-gray-400'>" . $albumSong->getDuration() . "</span>
 					</div>
 
 				</li>";
