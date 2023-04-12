f92b870ff664779322a073efa27d015307b6219d
Dillon R <dillonnollid@gmail.com>
2023-04-12 10:42:46 +0100

    Contribute pages now fully compatible and looking good on mobile and desktop.

--- create.php
+++ create.php
@@ -3,14 +3,13 @@ include("includes/includedFiles.php");
 ?>
 
 <h1 class="pageHeadingBig"></h1>
-<div class="items-center justify-center m-10 w-auto h-auto px-24 py-12 border-2 border-white ">
+<div class="items-center justify-center m-10 px-4 py-4 md:px-24 md:py-24 w-auto h-auto">
 
 	<?php if(isset($_GET["id"]) && trim($_GET["id"]) == 'song'){ ?>
 		<div class="items-center justify-center">
 
-			<h1 class="text-gray-600 dark:text-gray-400 text-center"><a href="https://ytmp3.cc/" target="blank">Add Song! <br>Click here go to youtube downloader. <br>Download MP3 in top quality and upload the file below!</a><br></h1>
+			<h1 class="text-gray-600 dark:text-gray-300 text-center"><a href="https://ytmp3.cc/" target="blank">Add Song!<br><br>Click here go to youtube downloader. <br>Download MP3 in top quality and upload the file below!</a><br></h1>
 			<br>
-            <hr>
 
 	        <form action="FileUpload.php" method="post" enctype="multipart/form-data">
 	        	<!--<input type="text" name="songSource" placeholder="Youtube URL (Not Available yet, use youtubeToMP3 link above)" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" disabled/>-->
@@ -52,15 +51,15 @@ include("includes/includedFiles.php");
 				</select>
 				<input type="hidden" name="add" value="song">
 	            <input type="file" name="upload" id="upload" placeholder="Choose File To Upload" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" required><br/>
-	            <hr>
+	            <br>
 	            <input type="submit" name="submit" value="Upload" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input">
 	        </form>
 		</div>
 
-		<?php echo "<br><hr><br>";
+		<?php echo "<br><hr>";
 		$songQuery = mysqli_query($con, "SELECT * FROM songs");
 			$i=1;
-			echo "<ul class='text-gray-600 dark:text-gray-400 text-center' style='padding:6%;text-size:20px;'>Existing Songs (Please don't duplicate)";
+			echo "<ul class='text-gray-600 dark:text-gray-300 text-center' style='padding:6%;text-size:20px;'><li>Existing Songs (Please don't duplicate)</li><br>";
 			while($row = mysqli_fetch_array($songQuery)) {
 				$name=$row['title'];
 				echo "<li>$name</li>";
@@ -79,8 +78,8 @@ include("includes/includedFiles.php");
 			?>
 	<?php } elseif(isset($_GET["id"]) && trim($_GET["id"]) == 'artist'){ ?>
 		<div class="items-center justify-center p-6">
-			<h1 class="text-gray-600 dark:text-gray-400 text-center">Add Artist! </h1>
-			<h2 class="text-gray-600 dark:text-gray-400 text-center">(Existing artists listed below)</h2>
+			<h1 class="text-gray-600 dark:text-gray-300 text-center">Add Artist! </h1>
+			<h2 class="text-gray-600 dark:text-gray-300 text-center">(Existing artists listed below)</h2><br>
 				
 	        <form action="FileUpload.php" method="post" enctype="multipart/form-data">
 				<input type="text" name="songArtist" placeholder="Artist Name" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" required/>
@@ -96,8 +95,8 @@ include("includes/includedFiles.php");
                         }
                     ?>
                 </select>
+				<br>
 				<input type="hidden" name="add" value="artist" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"/>
-	            <hr>
 	            <input type="submit" name="submit" value="Upload" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input">
 	        </form>
 		</div>
@@ -105,7 +104,7 @@ include("includes/includedFiles.php");
 		echo "<br><hr><br>";
 		$artistQuery = mysqli_query($con, "SELECT * FROM artists");
 			$i=1;
-			echo "<ul class='form-add-song' style='padding:6%;text-size:20px;'>Existing Artists (Please don't duplicate)";
+			echo "<ul class='text-gray-600 dark:text-gray-300 text-center' style='padding:6%;text-size:20px;'><li>Existing Artists (Please don't duplicate)</li><br>";
 			while($row = mysqli_fetch_array($artistQuery)) {
 				$name=$row['name'];
 				echo "<li>$name</li>";
@@ -114,8 +113,8 @@ include("includes/includedFiles.php");
 			echo "</ul>"; ?>
 	<?php } elseif(isset($_GET["id"]) && trim($_GET["id"]) == 'album'){ ?>
 		<div class="items-center justify-center p-6">
-			<h1 class="text-gray-600 dark:text-gray-400 text-center">Add Album! </h1>
-			<h2 class="text-gray-600 dark:text-gray-400 text-center">(Existing Albums listed below)</h2>
+			<h1 class="text-gray-600 dark:text-gray-300 text-center">Add Album! </h1>
+			<h2 class="text-gray-600 dark:text-gray-300 text-center">(Existing Albums listed below)</h2><br>
 				
 	        <form action="FileUpload.php" method="post" enctype="multipart/form-data">
 				<input type="text" name="albumTitle" placeholder="Album Title" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" required/>
@@ -144,7 +143,6 @@ include("includes/includedFiles.php");
 
 				<input type="hidden" name="add" value="album" />
 				<input type="file" name="imgupload" id="upload" placeholder="Choose Album artwork To Upload" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" required><br/>
-	            <hr>
 	            <input type="submit" name="submit" value="Upload" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input">
 	        </form>
 		</div>
@@ -152,7 +150,7 @@ include("includes/includedFiles.php");
 		<?php echo "<br><hr><br>";
 		$albumQuery = mysqli_query($con, "SELECT * FROM albums");
 			$i=1;
-			echo "<ul class='text-gray-600 dark:text-gray-400 text-center' style='padding:6%;text-size:20px;'>Existing Albums (Please don't duplicate)";
+			echo "<ul class='text-gray-600 dark:text-gray-300 text-center' style='padding:6%;text-size:20px;'><li>Existing Albums (Please don't duplicate)</li><br>";
 			while($row = mysqli_fetch_array($albumQuery)) {
 				$name=$row['title'];
 				echo "<li>$name</li>";
@@ -161,12 +159,12 @@ include("includes/includedFiles.php");
 			echo "</ul>";
 		} elseif(isset($_GET["id"]) && trim($_GET["id"]) == 'genre'){ ?>
 			<div class="items-center justify-center p-6">
-				<h1 class="text-gray-600 dark:text-gray-400 text-center">Add Genre!</h1>
-				<h2 class="text-gray-600 dark:text-gray-400 text-center">(Existing Genres listed below)</h2>
+				<h1 class="text-gray-600 dark:text-gray-300 text-center">Add Genre!</h1>
+				<h2 class="text-gray-600 dark:text-gray-300 text-center">(Existing Genres listed below)</h2><br>
 					
 		        <form action="FileUpload.php" method="post" enctype="multipart/form-data">
 					<input type="text" name="genre" placeholder="Genre Name (Please don't duplicate)" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" required/>
-		            <hr>
+		            <br>
 		            <input type="hidden" name="add" value="genre" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"/>
 		            <input type="submit" name="submit" value="Upload" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input">
 		        </form>
@@ -174,7 +172,7 @@ include("includes/includedFiles.php");
 
 		<?php echo "<br><hr><br>";
 		$genreQuery = mysqli_query($con, "SELECT * FROM genres");
-			echo "<ul class='form-add-song' style='padding:6%;text-size:20px;'>Existing Genres (Please don't duplicate)";
+			echo "<ul class='text-gray-600 dark:text-gray-300 text-center' style='padding:6%;text-size:20px;'><li>Existing Genres (Please don't duplicate)</li><br>";
 			while($row = mysqli_fetch_array($genreQuery)) {
 				$name=$row['name'];
 				echo "<li>$name</li>";
