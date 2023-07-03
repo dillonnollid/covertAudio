c52cd28f74bf7a30c0cf67f3a54858736333d869
Dillon Rosenkranz <dillonnollid@gmail.com>
2023-07-03 15:16:58 +0100

    Tinified images artwork and icons, meta tags for SEO

--- authenticate.php
+++ authenticate.php
@@ -21,6 +21,7 @@
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content="Authenticate for Underground Tracks">
 
     <title>covertAuth</title>
 
--- views/components/header.php
+++ views/components/header.php
@@ -37,6 +37,7 @@ else {
     <title>covertAudio</title>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Go Covert for Underground Tracks">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="assets/js/script.js"></script>
